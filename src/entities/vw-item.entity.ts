57cd7d52bb1fdec62b764bb3c4bc1a278fc687b9
Daniel Zamignani <danielzamignani@gmail.com>
2023-11-24 22:50:26 -0300

    feat: creating view for items

---
+++ src/entities/vw-item.entity.ts
@@ -0,0 +1,30 @@
+import { ViewColumn, ViewEntity } from "typeorm";
+
+@ViewEntity('vw_items')
+export class VwItemEntity {
+        @ViewColumn({
+            name: 'id'
+        })
+        id: number;
+      
+        @ViewColumn({
+            name: 'name'
+        })
+        name: string;
+      
+        @ViewColumn({
+            name: 'price'
+        })
+        price: number;
+      
+        @ViewColumn({
+            name: 'imageUrl'
+        })
+        imageUrl: string;
+
+
+      @ViewColumn({
+        name: 'stars'
+      })
+      stars: number     
+}
---
+++ src/shared/infra/typeorm/migrations/1700872279363-CreateVwItems.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateVwItems1700872279363 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE VIEW "vw_items" AS
+            SELECT 
+                i.id,
+                i.name,
+                i.price,
+                i."imageUrl",
+                ROUND(COALESCE(AVG(ir.stars), 0)) AS stars
+            FROM items i
+            LEFT JOIN items_ratings ir
+                ON ir."itemId" = i.id
+            GROUP BY i.id
+            ;  
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropView('vw_items');        
+    }
+
+}
+
