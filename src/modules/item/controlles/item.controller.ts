a558e8150566611253e0fa2e9c24d142fe13d30e
Daniel Zamignani <danielzamignani@gmail.com>
2023-11-26 12:56:59 -0300

    feat: get items by params

--- src/modules/item/controlles/item.controller.ts
+++ src/modules/item/controlles/item.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ItemService } from '../services/item.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from 'src/shared/decorators/current-user.decorator';
 import { ICurrentUser } from 'src/shared/interfaces/current-user.interface';
 import { GetItemResponseDTO } from '../dtos/get-item-response.dto';
+import { GetItemByQueryRequestDTO } from '../dtos/get-item-by-query.request.dto';
 
 @ApiTags('items')
 @Controller('items')
 export class ItemController {
   constructor(private readonly itemService: ItemService) {}
+
+  @ApiResponse({
+    status: 200,
+    type: [GetItemResponseDTO],
+    description: 'Return item by query',
+  })
+  @Get('/search')
+  async getItemByQuery(
+    @Query() getItemByQueryRequestDTO: GetItemByQueryRequestDTO,
+    @CurrentUser() currentUser: ICurrentUser,
+  ): Promise<GetItemResponseDTO[]> {
+    return this.itemService.getItemByQuery(getItemByQueryRequestDTO, currentUser);
+  }
 
   @ApiResponse({
     status: 200,
--- src/modules/item/services/item.service.ts
+++ src/modules/item/services/item.service.ts
@@ -1,15 +1,17 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { ICurrentUser } from 'src/shared/interfaces/current-user.interface';
 import { UserEntity } from 'src/entities/user.entity';
 import { VwItemEntity } from 'src/entities/vw-item.entity';
 import { GetItemResponseDTO } from '../dtos/get-item-response.dto';
 import { vwItemEntityToDtoMapper } from '../mappers/vwItem-entity-to-dto.mapper';
-
+import { GetItemByQueryRequestDTO } from '../dtos/get-item-by-query.request.dto';
+/**
+ * Instalar redis
+ */
 @Injectable()
 export class ItemService {
- 
   @InjectRepository(VwItemEntity)
   private readonly vWItemsEntityRepository: Repository<VwItemEntity>;
 
@@ -33,6 +35,29 @@ export class ItemService {
     }
 
     const getItemResponseDTO: GetItemResponseDTO = vwItemEntityToDtoMapper(item, favoriteUserItemsIds)
+
+    return getItemResponseDTO;
+  }
+
+  async getItemByQuery(getItemByQueryRequestDTO: GetItemByQueryRequestDTO, currentUser: ICurrentUser): Promise<GetItemResponseDTO[]> {
+    //REMOVER O LIKE 
+    const items: VwItemEntity[] = await this.vWItemsEntityRepository.createQueryBuilder('i')
+      .where(`LOWER(unaccent(i.name)) LIKE LOWER(unaccent('%${getItemByQueryRequestDTO.name}%'))`)
+      .getMany()
+
+    if (!items) throw new NotFoundException('Item not found!');
+
+    let favoriteUserItemsIds: number[] = [];
+    if (currentUser?.userId) {
+      favoriteUserItemsIds = await this.getFavoriteUserItems(
+        currentUser?.userId,
+      );
+    }
+
+    const getItemResponseDTO: GetItemResponseDTO[] = items.map((item) =>
+    vwItemEntityToDtoMapper(item, favoriteUserItemsIds),
+  );
+
 
     return getItemResponseDTO;
   }
